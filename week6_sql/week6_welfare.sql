d9516e8a2f1e575095b366b26a08140f7522bb14
MINSU-S <alstn6754@outlook.com>
2022-08-03 18:28:04 +0900

    0803_team_project

--- week6_sql/week6_welfare.sql
+++ week6_sql/week6_welfare.sql
@@ -1,2 +1,110 @@
 -- Active: 1658994301812@@127.0.0.1@3306@teamproject6
 select * from suicide_rate;
+select * from life_expectancy_at;
+
+--[ 외국과 비교한 우리나라 노령인구 실태 파악]
+--1.suicide_rate -- 정렬 및 색 bar
+--2.life_expendency_at -- 남,녀 테이블 나눠서 그 왼쪽,오른쪽 bar(수업시간에)
+
+--[자살률 지표] : 1.경제, 2. 외로움,고독
+--1.poverty_rate -- 정렬 및 색 bar
+--2.독거노인비율 -- plot 일단? 증가추세니까
+
+-- [ 은퇴시기 노령인구 상황 ] 임금,일자리,고용률 모두 최악인 상황에서 퇴직
+-- 테이블 세개, PLOT 한곳에
+-- 1.일자리 만족도, 임금만족도,고용률
+
+-- [ 현상황 ]: 령 생산가능인구 많고,의지도 있지만, 사회적지지가 많이 부족한상황 
+-- 1.워드클라우드: 일할 의지 높아보임
+-- 2. social_support : 분류3개로 테이블 나눠서 plot (중간 데이터 없는데 되나?!)
+-- 3.경제활동인구비율: 파이그래프 3개 SUPLOT
+-----> 노령인구 가장 많음.  (생산가능인구 / 경제활동인구 / 비겅제활동인구 ) 
+-----> 나라 생산가능인구 65세규정, 조정필요.
+
+
+# 1.suicide_rate -- 정렬 및 색 bar
+
+#2.life_expendaency_at -- 남,녀 테이블 나눠서 그 왼쪽,오른쪽 bar(수업시간에)
+SELECT LOCATION,Value FROM life_expectancy_at
+WHERE SUBJECT = 'MEN'
+ORDER BY Value DESC;
+SELECT LOCATION,Value FROM life_expectancy_at
+WHERE SUBJECT = 'WOMEN'
+ORDER BY Value DESC;
+
+SELECT LOCATION FROM life_expectancy
+WHERE SUBJECT = 'TOT' ORDER BY Value DESC;
+
+
+SELECT LOCATION, Value FROM social_support
+WHERE SUBJECT = 'YOUNG' ORDER BY Value DESC; 
+
+SELECT LOCATION, Value FROM social_support
+WHERE SUBJECT = 'MIDDLE_AGED' ORDER BY Value DESC; 
+
+SELECT LOCATION, Value FROM social_support
+WHERE SUBJECT = 'OLD' ORDER BY Value DESC; 
+
+select * from 일자리만족도;
+select * from 임금만족도;
+select * from 고용률;
+
+alter table suicide_rate
+ADD CONSTRAINT LOC_ID PRIMARY KEY(LOCATION);
+alter table 국가식별번호
+add constraint LOC_ID2 primary key (LOCATION);
+alter table suicide_rate
+add constraint LOC_ID3 foreign key (LOCATION) references 국가식별번호(LOCATION);
+
+alter table poverty_rate
+ADD CONSTRAINT LOC_ID PRIMARY KEY(LOCATION);
+alter table poverty_rate
+add constraint LOC_ID4 foreign key (LOCATION) references 국가식별번호(LOCATION);
+
+alter table life_expectancy
+ADD CONSTRAINT LOC_ID PRIMARY KEY(LOCATION);
+alter table life_expectancy
+add constraint LOC_ID5 foreign key (LOCATION) references 국가식별번호(LOCATION);
+
+
+alter table life_expectancy_at
+ADD CONSTRAINT LOC_ID PRIMARY KEY(LOCATION);
+alter table life_expectancy_at
+add constraint LOC_ID6 foreign key (LOCATION) references 국가식별번호(LOCATION);
+
+
+
+alter table social_support
+ADD CONSTRAINT LOC_ID PRIMARY KEY(LOCATION);
+alter table social_support
+add constraint LOC_ID7 foreign key (LOCATION) references 국가식별번호(LOCATION);
+
+
+
+SELECT 국가식별번호.country name FROM social_support	JOIN 국가식별번호	
+ON social_support.LOCATION = 국가식별번호.LOCATION
+WHERE SUBJECT='OLD' ;
+
+INSERT INTO 국가식별번호[countryname]
+values();
+
+
+alter table 임금만족도
+ADD CONSTRAINT ECO_ID2 PRIMARY KEY(age);
+alter table 경제활동인구비율
+add constraint ECO_ID primary key (age);
+alter table 임금만족도
+add constraint ECO_ID3 foreign key (age) references 경제활동인구비율(age);
+
+alter table 일자리만족도
+ADD CONSTRAINT ECO_ID4 PRIMARY KEY(age);
+
+alter table 일자리만족도
+add constraint ECO_ID5 foreign key (age) references 경제활동인구비율(age);
+
+alter table 고용률
+ADD CONSTRAINT ECO_ID6 PRIMARY KEY(age);
+
+alter table 고용률
+add constraint ECO_ID7 foreign key (age) references 경제활동인구비율(age);
+
